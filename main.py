ed1eeb18ce544028214d3b0b7742aed038cb06b6
vn-ki <appukuttancr@gmail.com>
2017-12-06 17:22:12 +0530

    Revamped UI

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import gi
 import subprocess
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
+'''
 
+cssProvider = Gtk.CssProvider()
+cssProvider.load_from_path('style.css')
+screen = Gdk.Screen.get_default()
+styleContext = Gtk.StyleContext()
+styleContext.add_provider_for_screen(screen, cssProvider,
+                                     Gtk.STYLE_PROVIDER_PRIORITY_USER)
+'''
 import youtubeplayer
 
 window = youtubeplayer.YouTubePlayer()
--- youtubeplayer.py
+++ youtubeplayer.py
@@ -6,7 +6,7 @@ import threading
 import os
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gio
 
 class YouTubePlayer(Gtk.Window) :
     def __init__(self) :
@@ -15,66 +15,92 @@ class YouTubePlayer(Gtk.Window) :
         self.vlcShell = None
         self.downloadThread = None
 
-        Gtk.Window.__init__(self, title="YouTube Player")
+        Gtk.Window.__init__(self, title="YouTubePlayer")
         self.set_border_width(10)
-        self.set_default_size(400, 100)
+        self.set_size_request(500, 100)
+
+        #Title bar tweaks
+        headerBar = Gtk.HeaderBar()
+        headerBar.set_show_close_button(True)
+        headerBar.props.title = ""
+        self.set_titlebar(headerBar)
+        #
 
         ####################################################################
         ## Main Box: All widgets are inside this
-        self.mainBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=10)
+        mainBox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=10)
         ##
 
         #Label
 
         self.infoLabel = Gtk.Label("YouTubePlayer")
-        self.mainBox.pack_start(self.infoLabel, True, True, 0)
+        mainBox.pack_start(self.infoLabel, True, True, 0)
         #
 
         #Input box
         self.entry = Gtk.Entry()
         self.entry.set_placeholder_text("URL")
-        self.mainBox.pack_start(self.entry, True, True, 0)
+        mainBox.pack_start(self.entry, True, True, 0)
 
 
         #############################################################
         #Button for 2nd line, checkbox and download button
         #
         #Check box
-        self.checkButtonBox = Gtk.Box(spacing=10)
-        self.mainBox.pack_start(self.checkButtonBox, True, True, 0)
+        checkButtonBox = Gtk.Box(spacing=10)
+        mainBox.pack_start(checkButtonBox, True, True, 0)
         self.audioOnlyButton = Gtk.CheckButton("Audio only")
         self.audioOnlyButton.connect('toggled', self.audioOnly)
-        self.checkButtonBox.pack_start(self.audioOnlyButton, True, True,0)
+        checkButtonBox.pack_start(self.audioOnlyButton, True, True,0)
+
+
+        #Download and info Box
+        dliBox = Gtk.Box(spacing=10)
+        checkButtonBox.pack_start(dliBox, True, True, 0)
 
         #Download button
-        self.downloadButton = Gtk.Button(label='Download')
+        img = Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="document-save-symbolic"), Gtk.IconSize.BUTTON)
+        self.downloadButton = Gtk.Button( image=img, name='download-button')
+        self.downloadButton.set_property("width-request", 30)
         self.downloadButton.connect('clicked', self.download)
-        self.checkButtonBox.pack_start(self.downloadButton, True,True,0)
+        dliBox.pack_start(self.downloadButton, True,True,0)
+
+        img = Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="help-about-symbolic"), Gtk.IconSize.BUTTON)
+        self.downloadButton = Gtk.Button( image=img, name='download-button')
+        self.downloadButton.set_property("width-request", 30)
+        self.downloadButton.connect('clicked', self.download)
+        dliBox.pack_start(self.downloadButton, True,True,0)
         #############################################################
 
         #############################################################
         #Box for lower buttons
         #
 
-        self.buttonBox = Gtk.Box(spacing=10)
-        self.buttonBox.set_border_width(20)
-        self.mainBox.pack_start(self.buttonBox, True,True, 0)
-        self.add(self.mainBox)
+        self.buttonBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(self.buttonBox.get_style_context(), "linked")
+    #    self.mainBox.pack_start(self.buttonBox, True,True, 0)
+        self.add(mainBox)
 
         #Previous button
-        self.prevButton = Gtk.Button(label='Prev')
+        img = Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-skip-backward-symbolic"), Gtk.IconSize.BUTTON)
+        self.prevButton = Gtk.Button(image=img, name='prev-button')
         self.prevButton.connect('clicked', self.previous)
         self.buttonBox.pack_start(self.prevButton, True, True, 0)
 
         #Play button
-        self.playButton = Gtk.Button(label='Play')
+    #    img = Gtk.Image.new_from_file("images/icons/youtube-icon.png")
+        img = Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-playback-start-symbolic"), Gtk.IconSize.BUTTON)
+        self.playButton = Gtk.Button(image=img, name='play-button')
         self.playButton.connect('clicked', self.play)
         self.buttonBox.pack_start(self.playButton, True, True, 0)
 
         #Next button
-        self.nextButton = Gtk.Button(label='Next')
+        img = Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-skip-forward-symbolic"), Gtk.IconSize.BUTTON)
+        self.nextButton = Gtk.Button(image=img, name='next-button')
         self.nextButton.connect('clicked', self.next)
         self.buttonBox.pack_start(self.nextButton, True, True, 0)
+
+        headerBar.pack_start(self.buttonBox)
         #############################################################
 
     def play(self, widget) :
